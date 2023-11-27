630914f8e1955a0236e0591aa2c2bb49b9d75b8d
Jesse Lee Ross <jesseross001@gmail.com>
2023-11-27 13:31:16 +0000

    Added login forms view and template

--- user1/views.py
+++ user1/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import StaffUserCreationForm, GeneralUserCreationForm
 
+def register(request,user_type):
+    if user_type == 'staff':
+        form_class = StaffUserCreationForm
+    else:
+        form_class =GeneralUserCreationForm
+
+    if request.method =='POST':
+        form = form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = form_class()
+    return render(request,'user1/registration.html',{'form':form})
 # Create your views here.
