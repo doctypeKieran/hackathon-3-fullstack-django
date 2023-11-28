11a23f452d616fab468d3260ee5fddf410a0ff79
Kieran Hillman <developedbykieran@gmail.com>
2023-11-28 10:11:17 +0000

    Add django whitenoise to correct place in middleware

--- my_project/settings.py
+++ my_project/settings.py
@@ -54,6 +54,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
