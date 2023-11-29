ec711daaa5a4abaa9015ecd46f41dc5c69b2441b
Kieran Hillman <developedbykieran@gmail.com>
2023-11-29 11:20:01 +0000

    Update models classes and functions with two empty lines

--- event/models.py
+++ event/models.py
@@ -17,6 +17,7 @@ GENERAL_USER = 'USER'
 # Create your models here.
 # Adding Models in from ideation 
 
+
 class RageRoomSession(models.Model):
     facilitator = models.ForeignKey('user1.UserProfile', on_delete=models.SET_NULL, null=True, related_name='facilitated_sessions')
     title = models.CharField(max_length=200)
@@ -27,18 +28,24 @@ class RageRoomSession(models.Model):
     capacity = models.IntegerField(default=10)  
     image = models.ImageField(upload_to='rage_room_images/', blank=True, null=True)
     featured_image = CloudinaryField('image', default='placeholder', null=True, blank=True)
+
+
     def __str__(self):
         return f"{self.title} on {self.date}"
+
 
     def save(self, *args, **kwargs):
         if self.capacity < 0:
             raise ValidationError("Capacity cannot be negative")
         super().save(*args, **kwargs)
+
+
 class Booking(models.Model):
     session = models.ForeignKey(RageRoomSession, on_delete=models.CASCADE, related_name='bookings')
     participant = models.ForeignKey('user1.UserProfile', on_delete=models.CASCADE, related_name='bookings')
     booking_time = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+
 
     def save(self, *args, **kwargs):
      super(Booking, self).save(*args, **kwargs)
@@ -53,9 +60,5 @@ class Booking(models.Model):
                 self.save()
             elif self.session.capacity <= 0:
                 raise ValidationError('No more capacity available for this session.')
-
         else:
             raise ValidationError(_('Only staff members can approve bookings.'))
-    
-    
-
